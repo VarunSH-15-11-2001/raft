60dd3ccfa4040097ec9083ac8581786009ab0835
Varun Hoskere <varunshankarhoskere@Varuns-MacBook-Pro.local>
2025-10-28 19:22:49 -0600

    add all message structures

--- message/message.go
+++ message/message.go
@@ -1,30 +1,32 @@
 package message
 
 import (
-	"consensus-kvstore/node"
+	"consensus-kvstore/log"
 )
 
-type RequestMessage struct {
-	FromID       int
-	Term         int
-	ToID         int
-	LastLogIndex int
-	LastLogTerm  int
+type VoteRequest struct {
+	CandidateTerm int
+	CandidateID   int
+	LastLogIndex  int
+	LastLogTerm   int
 }
 
-type ResponseMessage struct {
-	FromID       int
-	Term         int
-	VotedGranted bool
+type VoteResponse struct {
+	Term        int
+	VoteGranted bool
 }
 
-func CreateVoteRequest(from_node *node.Node, to_node *node.Node) *RequestMessage {
-	requestMessage := &RequestMessage{
-		FromID:       from_node.ID,
-		Term:         from_node.currentTerm,
-		ToID:         to_node.ID,
-		LastLogIndex: from_node.LastLogIndex,
-		LastLogTerm:  from_node.LastLogIndex,
-	}
-	return requestMessage
+type AppendEntriesRequest struct {
+	LeaderTerm   int
+	LeaderID     int
+	Entries      []log.LogEntry
+	PrevLogIndex int
+	PrevLogTerm  int
+	LeaderCommit int
+}
+
+type AppenEntriesResponse struct {
+	FollowerTerm int
+	Success      bool
+	Index        int
 }
