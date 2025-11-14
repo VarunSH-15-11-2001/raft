69c45ed6de9efa2dd30436b34334d0f1f73b3ef5
Varun Hoskere <varunshankarhoskere@Varuns-MacBook-Pro.local>
2025-11-13 20:31:52 -0700

    add logger to node structure

--- main.go
+++ main.go
@@ -4,26 +4,49 @@ import (
 	"consensus-kvstore/communication_impl"
 	"consensus-kvstore/node"
 	"fmt"
+	"log/slog"
+	"os"
 	"sync"
 	"time"
 )
 
 func main() {
-	fmt.Println("main")
-	electionTimeout := 6 * time.Second
+
+	file, err := os.OpenFile(
+		"logs/main.log",
+		os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
+		0666,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	logger_main := slog.New(slog.NewJSONHandler(file, nil))
+
+	logger_main.Info("Main started")
+	leader_logger, leader_file := GetLogger("logs/leader.log")
+	electionTimeout := 20 * time.Second
 	heartbeatTimeout := 5 * time.Second
 	electionTimer := time.NewTimer(electionTimeout)
 	heartbeatTimer := time.NewTimer(heartbeatTimeout)
-	leader := node.NewNode(0, 0, node.Leader, time.Second*2, electionTimer, time.Second*4, heartbeatTimer)
+	leader := node.NewNode(0, 0, node.Leader, time.Second*20, electionTimer, time.Second*4, heartbeatTimer, leader_logger)
 	leader.State = node.Leader
-	fmt.Printf("\nCreated Node: %+v\n", leader)
 
-	follower1 := GetNewNode(1)
+	logger_main.Info("Created Leader Node with ID:", "id", leader.ID)
+
+	follower1_logger, follower1_file := GetLogger("logs/follower1.log")
+	follower1 := GetNewNode(1, follower1_logger)
 	// follower1_timer := time.NewTimer(1 * time.Second)
 	// follower1.electionTimeout = 1 * time.Second
 
-	follower2 := GetNewNode(2)
-	follower3 := GetNewNode(3)
+	follower2_logger, follower2_file := GetLogger("logs/follower2.log")
+	follower2 := GetNewNode(2, follower2_logger)
+
+	follower3_logger, follower3_file := GetLogger("logs/follower3.log")
+	follower3 := GetNewNode(3, follower3_logger)
 
 	var nodeMap map[int]*node.Node = make(map[int]*node.Node)
 	nodeMap[leader.ID] = leader
@@ -37,7 +60,10 @@ func main() {
 	follower1_done := make(chan bool)
 	follower2_done := make(chan bool)
 	follower3_done := make(chan bool)
+	leader.Listener(&network)
 	follower1.Listener(&network)
+	follower2.Listener(&network)
+	follower3.Listener(&network)
 
 	var wg sync.WaitGroup
 
@@ -77,22 +103,47 @@ func main() {
 	wg.Wait()
 
 	if len(network.History) >= 2 {
-		fmt.Printf("\nMessage added from nodeID:%d to nodeID:%d\n", network.History[1].FromID, network.History[1].ToID)
+		logger_main.Debug("Message added", "fromID", network.History[1].FromID, "toID", network.History[1].ToID)
 	}
 
-	time.Sleep(8 * time.Second)
-	fmt.Print("\nMain completed\n")
+	time.Sleep(30 * time.Second)
+	defer file.Close()
+	defer leader_file.Close()
+	defer follower1_file.Close()
+	defer follower2_file.Close()
+	defer follower3_file.Close()
+
+	defer fmt.Print("Main completed")
 }
 
-func GetNewNode(i int) *node.Node {
+func GetNewNode(i int, syslog *slog.Logger) *node.Node {
 	IDs := []int{0, 1, 2, 3, 4}
 	var Term int = 0
 
-	ElectionTimeouts := []time.Duration{2 * time.Second, 3 * time.Second, 4 * time.Second, 5 * time.Second}
+	ElectionTimeouts := []time.Duration{20 * time.Second, 2 * time.Second, 6 * time.Second, 10 * time.Second, 15 * time.Second}
 	electionTimer := time.NewTimer(ElectionTimeouts[i])
 
-	fmt.Printf("\nCreated Node: %+v with electionTimeout: %+v\n", IDs[i], ElectionTimeouts[i])
+	// logger_main.Info("\nCreated Node: %+v with electionTimeout: %+v\n", IDs[i], ElectionTimeouts[i])
 
-	return node.NewNode(IDs[i], Term, node.Follower, ElectionTimeouts[i], electionTimer, 1*time.Second, time.NewTimer(1*time.Second))
+	return node.NewNode(IDs[i], Term, node.Follower, ElectionTimeouts[i], electionTimer, 1*time.Second, time.NewTimer(1*time.Second), syslog)
 
+}
+
+func GetLogger(nodeName string) (*slog.Logger, *os.File) {
+	file, err := os.OpenFile(
+		nodeName,
+		os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
+		0666,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	opts := &slog.HandlerOptions{
+		Level:     slog.LevelInfo,
+		AddSource: false,
+	}
+
+	return slog.New(slog.NewTextHandler(file, opts)), file
 }
--- node/node.go
+++ node/node.go
@@ -5,6 +5,7 @@ import (
 	"consensus-kvstore/log"
 	"consensus-kvstore/message"
 	"fmt"
+	"log/slog"
 	"time"
 )
 
@@ -26,7 +27,7 @@ func Log(message string) {
 
 type Node struct {
 	ID               int
-	currentTerm      int
+	CurrentTerm      int
 	State            NodeState
 	electionTimeout  time.Duration
 	heartbeatTimeout time.Duration
@@ -35,19 +36,21 @@ type Node struct {
 	Log_Node         *log.Log
 	Inbox            chan *message.Envelope
 	Voted            bool
+	SystemLog        *slog.Logger
 }
 
-func NewNode(id int, currentTerm int, state NodeState, electionTimeout time.Duration, electionTimer *time.Timer,
-	heartbeatTimeout time.Duration, heartbeatTimer *time.Timer) *Node {
+func NewNode(id int, CurrentTerm int, state NodeState, electionTimeout time.Duration, electionTimer *time.Timer,
+	heartbeatTimeout time.Duration, heartbeatTimer *time.Timer, syslog *slog.Logger) *Node {
 	Log_Object := &log.Log{
 		LastLogTerm: 0,
 		CommitIndex: 0,
 		LastApplied: 0,
 		List:        []log.LogEntry{},
 	}
+	syslog.Info(fmt.Sprintf("Created node %d", id))
 	return &Node{
 		ID:               id,
-		currentTerm:      currentTerm,
+		CurrentTerm:      CurrentTerm,
 		State:            state,
 		electionTimeout:  electionTimeout,
 		electionTimer:    electionTimer,
@@ -56,6 +59,7 @@ func NewNode(id int, currentTerm int, state NodeState, electionTimeout time.Dura
 		Log_Node:         Log_Object,
 		Inbox:            make(chan *message.Envelope, 100),
 		Voted:            false,
+		SystemLog:        syslog,
 	}
 }
 
@@ -68,7 +72,8 @@ func StartElectionTimeout(n *Node, done chan<- bool, net communication_iface.Net
 		<-n.electionTimer.C
 		done <- true
 		StartElection(n, net)
-		fmt.Printf("\nelection timeout for node %d, starting leader election", n.ID)
+		n.SystemLog.Info(fmt.Sprintf("election timeout for node %d, starting leader election", n.ID))
+		// fmt.Printf("\nelection timeout for node %d, starting leader election", n.ID)
 	}()
 }
 
@@ -84,24 +89,32 @@ func StartElection(n *Node, net communication_iface.Network) {
 	*/
 
 	n.State = Candidate
-	n.currentTerm++
-	fmt.Printf("\nNode %d became candidate for term %d", n.ID, n.currentTerm)
-	RequestVotes(n, net)
-
+	n.CurrentTerm++
+	n.SystemLog.Info(fmt.Sprintf("Node %d became candidate for term %d", n.ID, n.CurrentTerm))
+	// fmt.Printf("\nNode %d became candidate for term %d", n.ID, n.CurrentTerm)
+	net.RequestVotes(n.ID)
+	// net.RequestVotes(candiadte, net)
 	// majority:=TallyVotes()
 	// if majority, MakeLeader()
 
 }
 
-func RequestVotes(candidate *Node, net communication_iface.Network) {
-	VoteRequest := &message.VoteRequest{
-		CandidateTerm: candidate.currentTerm,
-		CandidateID:   candidate.ID,
-		LastLogIndex:  len(candidate.Log_Node.List),
-	}
-	// send vote requests to ALL nodes in netmap
-	net.SendVoteRequest(candidate.ID, 1, *VoteRequest)
+/*
+func RequestVotes() {
+	for nodes in nodemaP:
+		SendVoteRequest()
 }
+*/
+
+// func RequestVotes(candidate *Node, net communication_iface.Network) {
+// 	VoteRequest := &message.VoteRequest{
+// 		CandidateTerm: candidate.currentTerm,
+// 		CandidateID:   candidate.ID,
+// 		LastLogIndex:  len(candidate.Log_Node.List),
+// 	}
+// 	// send vote requests to ALL nodes in netmap
+// 	net.SendVoteRequest(candidate.ID, 1, *VoteRequest)
+// }
 
 func (n *Node) Listener(net communication_iface.Network) {
 	go func() {
@@ -109,7 +122,7 @@ func (n *Node) Listener(net communication_iface.Network) {
 			msg, ok := <-n.Inbox
 			if ok {
 				n.HandleMessage(msg, net)
-				fmt.Printf("\nnode %d received a message!\n", n.ID)
+				n.SystemLog.Info(fmt.Sprintf("node %d received a message!\n", n.ID))
 			}
 		}
 
@@ -120,20 +133,34 @@ func (n *Node) Listener(net communication_iface.Network) {
 func (n *Node) HandleMessage(msg *message.Envelope, net communication_iface.Network) {
 	if msg.Type == "VoteRequest" {
 		VoteResponse := message.VoteResponse{
-			Term:        n.currentTerm,
-			VoteGranted: true,
+			Term:        n.CurrentTerm,
+			VoteGranted: false,
+		}
+
+		req := msg.Payload.(message.VoteRequest)
+		if req.CandidateTerm < n.Log_Node.LastLogTerm {
+			n.SystemLog.Info(fmt.Sprintf("[Node %d] Reject vote: candidate id:%d term:%d < my term %d", n.ID, req.CandidateID, req.CandidateTerm, n.CurrentTerm))
+			net.SendVoteResponse(n.ID, msg.FromID, VoteResponse)
 		}
 
 		if n.Voted {
-			VoteResponse.VoteGranted = false
+			n.SystemLog.Info(fmt.Sprintf("[Node %d] Reject vote: already voted in term %d", n.ID, n.CurrentTerm))
+			net.SendVoteResponse(n.ID, msg.FromID, VoteResponse)
+			return
 		}
 
-		// fromNode := net.Nodes[msg.FromID]
-		// if fromNode.Log_Node.LastLogTerm < n.Log_Node.LastLogTerm {
-		// 	VoteResponse.VoteGranted = false
-		// }
+		if req.LastLogTerm < n.Log_Node.LastLogTerm ||
+			(req.LastLogTerm == n.Log_Node.LastLogTerm && req.LastLogIndex < len(n.Log_Node.List)) {
+			n.SystemLog.Info(fmt.Sprintf("[Node %d] Reject vote: candidate pair log term, log index:%d, %d < my pair %d,%d", n.ID, req.LastLogTerm, req.LastLogIndex, n.Log_Node.LastLogTerm, len(n.Log_Node.List)))
+			net.SendVoteResponse(n.ID, msg.FromID, VoteResponse)
+			return
+		}
 
+		n.Voted = true
+		VoteResponse.VoteGranted = true
+		n.SystemLog.Info(fmt.Sprintf("[Node %d] Accept vote", n.ID))
 		net.SendVoteResponse(n.ID, msg.FromID, VoteResponse)
+		return
 	}
 }
 
