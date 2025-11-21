a89af52f8650e2732088753e2e1a3328c8059a65
Varun Hoskere <varunshankarhoskere@Varuns-MacBook-Pro.local>
2025-11-21 14:32:10 -0700

    add AppenEntry RPC

--- node/node.go
+++ node/node.go
@@ -37,6 +37,7 @@ type Node struct {
 	Inbox            chan *message.Envelope
 	Voted            bool
 	SystemLog        *slog.Logger
+	Votes            int
 }
 
 func NewNode(id int, CurrentTerm int, state NodeState, electionTimeout time.Duration, electionTimer *time.Timer,
@@ -60,6 +61,7 @@ func NewNode(id int, CurrentTerm int, state NodeState, electionTimeout time.Dura
 		Inbox:            make(chan *message.Envelope, 100),
 		Voted:            false,
 		SystemLog:        syslog,
+		Votes:            0,
 	}
 }
 
@@ -132,6 +134,13 @@ func (n *Node) Listener(net communication_iface.Network) {
 
 func (n *Node) HandleMessage(msg *message.Envelope, net communication_iface.Network) {
 	if msg.Type == "VoteRequest" {
+		/*
+			Receiver implementation:
+			1. Reply false if term < currentTerm (§5.1)
+			2. If votedFor is null or candidateId, and candidate’s log is at
+			least as up-to-date as receiver’s log, grant vote (§5.2, §5.4)
+		*/
+
 		VoteResponse := message.VoteResponse{
 			Term:        n.CurrentTerm,
 			VoteGranted: false,
@@ -140,26 +149,119 @@ func (n *Node) HandleMessage(msg *message.Envelope, net communication_iface.Netw
 		req := msg.Payload.(message.VoteRequest)
 		if req.CandidateTerm < n.Log_Node.LastLogTerm {
 			n.SystemLog.Info(fmt.Sprintf("[Node %d] Reject vote: candidate id:%d term:%d < my term %d", n.ID, req.CandidateID, req.CandidateTerm, n.CurrentTerm))
-			net.SendVoteResponse(n.ID, msg.FromID, VoteResponse)
+			net.SendVoteResponse(n.ID, msg.FromID, 1, VoteResponse)
 		}
 
 		if n.Voted {
 			n.SystemLog.Info(fmt.Sprintf("[Node %d] Reject vote: already voted in term %d", n.ID, n.CurrentTerm))
-			net.SendVoteResponse(n.ID, msg.FromID, VoteResponse)
+			net.SendVoteResponse(n.ID, msg.FromID, 1, VoteResponse)
 			return
 		}
 
 		if req.LastLogTerm < n.Log_Node.LastLogTerm ||
 			(req.LastLogTerm == n.Log_Node.LastLogTerm && req.LastLogIndex < len(n.Log_Node.List)) {
 			n.SystemLog.Info(fmt.Sprintf("[Node %d] Reject vote: candidate pair log term, log index:%d, %d < my pair %d,%d", n.ID, req.LastLogTerm, req.LastLogIndex, n.Log_Node.LastLogTerm, len(n.Log_Node.List)))
-			net.SendVoteResponse(n.ID, msg.FromID, VoteResponse)
+			net.SendVoteResponse(n.ID, msg.FromID, 1, VoteResponse)
 			return
 		}
 
 		n.Voted = true
 		VoteResponse.VoteGranted = true
 		n.SystemLog.Info(fmt.Sprintf("[Node %d] Accept vote", n.ID))
-		net.SendVoteResponse(n.ID, msg.FromID, VoteResponse)
+		net.SendVoteResponse(n.ID, msg.FromID, 1, VoteResponse)
+		return
+	} else if msg.Type == "VoteResponse" {
+		resp := msg.Payload.(message.VoteResponse)
+
+		if resp.Term > n.CurrentTerm {
+			n.CurrentTerm = resp.Term
+			n.State = Follower
+			n.Voted = false
+			return
+		}
+
+		if resp.VoteGranted && n.State == Candidate {
+			n.Votes++
+
+			if n.Votes > net.GetNumberOfNodes()/2 {
+				n.State = Leader
+				n.SystemLog.Info(fmt.Sprintf("[Node %d] Became leader for term %d", n.ID, n.CurrentTerm))
+				// net.HeartBeat(n.ID)
+			}
+		}
+
+	} else if msg.Type == "AppendEntry" {
+		/*
+			Receiver implementation:
+				[x] 1. Reply false if term < currentTerm (§5.1)
+				[x] 2. Reply false if log doesn’t contain an entry at prevLogIndex whose term matches prevLogTerm (§5.3)
+					(consistency check to confirm that the follower is atleast upto date with leader)
+				[x] 3. If an existing entry conflicts with a new one (same index but different terms),
+					delete the existing entry and all that follow it (§5.3)
+				[x] 4. Append any new entries not already in the log
+				[x] 5. If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index of last new entry)
+		*/
+
+		req := msg.Payload.(message.AppendEntriesRequest)
+		AppendEntryPayload := message.AppendEntriesResponse{
+			FollowerTerm: n.CurrentTerm,
+			Success:      false,
+			Index:        0,
+		}
+
+		if n.CurrentTerm > req.LeaderTerm {
+			net.SendAppendEntriesResponse(n.ID, req.LeaderID, AppendEntryPayload)
+			n.SystemLog.Info(fmt.Sprintf("[Node %d] Didn't append because my term %d is newer than leader %d", n.ID, n.CurrentTerm, req.LeaderTerm))
+			return
+		}
+
+		if req.PrevLogIndex > len(n.Log_Node.List)-1 || n.Log_Node.List[req.PrevLogIndex].Term != req.PrevLogTerm {
+			net.SendAppendEntriesResponse(n.ID, req.LeaderID, AppendEntryPayload)
+			n.SystemLog.Info(fmt.Sprintf("[Node %d] Didn't append because I don't have an entry at prevLogIndex %d with term %d", n.ID, req.PrevLogIndex, req.PrevLogTerm))
+			return
+		}
+
+		conflictIndex := -1
+		for i, entry := range req.Entries {
+			targetIndex := req.PrevLogIndex + i + 1
+
+			if targetIndex > len(n.Log_Node.List) {
+				conflictIndex = i
+				break
+			}
+
+			if targetIndex < len(n.Log_Node.List) {
+				if n.Log_Node.List[targetIndex].Term != entry.Term {
+					n.Log_Node.List = n.Log_Node.List[:targetIndex]
+					break
+				}
+			}
+		}
+
+		if conflictIndex != -1 {
+			n.Log_Node.List = append(n.Log_Node.List, req.Entries[conflictIndex:]...)
+		} else {
+			startIndex := req.PrevLogIndex + 1
+			if startIndex < len(req.Entries) {
+				missing := req.Entries[startIndex-req.PrevLogIndex:]
+				n.Log_Node.List = append(n.Log_Node.List, missing...)
+			}
+		}
+
+		if req.LeaderCommit > n.Log_Node.CommitIndex {
+			lastEntryCommit := n.Log_Node.List[len(n.Log_Node.List)-1].Index
+			n.Log_Node.CommitIndex = min(req.LeaderCommit, lastEntryCommit)
+			n.SystemLog.Info(
+				fmt.Sprintf(
+					"[Node %d] Updated commitIndex to %d (leaderCommit=%d, lastEntry=%d)",
+					n.ID, n.Log_Node.CommitIndex, req.LeaderCommit, lastEntryCommit,
+				),
+			)
+		}
+
+		AppendEntryPayload.Success = true
+		AppendEntryPayload.Index = len(n.Log_Node.List) - 1
+		net.SendAppendEntriesResponse(n.ID, req.LeaderID, AppendEntryPayload)
 		return
 	}
 }
